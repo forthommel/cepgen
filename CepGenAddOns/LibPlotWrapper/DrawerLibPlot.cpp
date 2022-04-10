6b0d0d92c085c5e2a59910018af92f85b6d9474a
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-04-11 01:49:10 +0200

    Plotutils: dropped skeletton

--- CepGenAddOns/LibPlotWrapper/DrawerLibPlot.cpp
+++ CepGenAddOns/LibPlotWrapper/DrawerLibPlot.cpp
@@ -1,3 +1,21 @@
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2022-2023  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <plotter.h>
 
 #include "CepGen/Core/Exception.h"
@@ -18,51 +36,37 @@ namespace cepgen {
 
       const DrawerLibPlot& draw(const DrawableColl&,
                                 const std::string& name = "",
+                                const std::string& title = "",
                                 const Mode& mode = Mode::none) const override;
     };
 
     DrawerLibPlot::DrawerLibPlot(const ParametersList& params) : Drawer(params) {}
 
     const DrawerLibPlot& DrawerLibPlot::draw(const Graph1D& graph, const Mode&) const {
-      auto gr = convert(graph);
-      ROOTCanvas canv(graph.name(), graph.title());
-      gr.Draw("al");
-      canv.Prettify(gr.GetHistogram());
-      canv.Save("pdf");
+      CG_WARNING("DrawerLibPlot:draw") << "Not yet implemented.";
       return *this;
     }
 
     const DrawerLibPlot& DrawerLibPlot::draw(const Graph2D& graph, const Mode&) const {
-      auto gr = convert(graph);
-      ROOTCanvas canv(graph.name());
-      gr.Draw("colz");
-      canv.Prettify(gr.GetHistogram());
-      canv.Save("pdf");
+      CG_WARNING("DrawerLibPlot:draw") << "Not yet implemented.";
       return *this;
     }
 
     const DrawerLibPlot& DrawerLibPlot::draw(const Hist1D& hist, const Mode&) const {
-      auto h = convert(hist);
-      ROOTCanvas canv(hist.name(), hist.title());
-      h.Draw();
-      canv.Prettify(&h);
-      canv.Save("pdf");
+      CG_WARNING("DrawerLibPlot:draw") << "Not yet implemented.";
       return *this;
     }
 
     const DrawerLibPlot& DrawerLibPlot::draw(const Hist2D& hist, const Mode&) const {
-      auto h = convert(hist);
-      ROOTCanvas canv(hist.name(), hist.title());
-      h.Draw("colz");
-      canv.Prettify(&h);
-      canv.Save("pdf");
+      CG_WARNING("DrawerLibPlot:draw") << "Not yet implemented.";
       return *this;
     }
 
     const DrawerLibPlot& DrawerLibPlot::draw(const DrawableColl& objs,
                                              const std::string& name,
+                                             const std::string& title,
                                              const Mode& mode) const {
-      TMultiGraph mg;
+      /*TMultiGraph mg;
       THStack hs;
       ROOTCanvas canv(name.c_str(), "");
       size_t i = 0;
@@ -92,7 +96,8 @@ namespace cepgen {
         canv.Prettify(hs.GetHistogram());
       else if (has_graphs)
         canv.Prettify(mg.GetHistogram());
-      canv.Save("pdf");
+      canv.Save("pdf");*/
+      CG_WARNING("DrawerLibPlot:draw") << "Not yet implemented.";
       return *this;
     }
   }  // namespace utils
