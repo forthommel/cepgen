7e359bdd00b00f588ab9698d4976567c98d22ecd
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-12-07 14:14:41 +0100

    MG5: add capability to define particles aliases

---
+++ CepGenAddOns/MadGraphWrapper/test/add_particles.cc
@@ -0,0 +1,24 @@
+#include "CepGen/Core/Exception.h"
+#include "CepGen/Generator.h"
+#include "CepGen/Modules/ProcessFactory.h"
+#include "CepGen/Physics/PDG.h"
+#include "CepGen/Process/Process.h"
+#include "CepGenAddOns/MadGraphWrapper/MadGraphDummyProcess.hxx"
+#include "CepGenAddOns/MadGraphWrapper/MadGraphProcess.h"
+
+int main() {
+  cepgen::initialise();
+
+  const cepgen::pdgid_t my_part = 13;
+
+  cepgen::PDG::get().define(cepgen::ParticleProperties(my_part, "la", "laurentino", 0., 42., 0., 3, true));
+  CG_LOG << cepgen::PDG::get()(my_part);
+
+  auto mg5 = cepgen::proc::ProcessFactory::get().build(
+      "mg5_aMC",
+      cepgen::ParametersList()
+          .set("extraParticles", cepgen::ParametersList().set("la", cepgen::PDG::get()(my_part)))
+          .set<std::string>("process", "a a > la+ la-"));
+
+  return 0;
+}
