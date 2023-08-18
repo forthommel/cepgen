0cad9b024ebf029a43bfb88117855d8ffd014a53
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-08-18 09:12:47 +0300

    Process: simplification of process accessor in PS generator

--- CepGen/Process/CollinearPhaseSpaceGenerator.cpp
+++ CepGen/Process/CollinearPhaseSpaceGenerator.cpp
@@ -77,7 +77,7 @@ namespace cepgen {
       mpart1_ = PDG::get().mass(pos_flux_->partonPdgId());
       mpart2_ = PDG::get().mass(neg_flux_->partonPdgId());
 
-      const auto log_lim_q2 = kin.cuts().initial.q2.truncate(Limits{1.e-5, 5.}).compute(std::log);
+      const auto log_lim_q2 = kin.cuts().initial.q2.truncate(Limits{1.e-5, 50.}).compute(std::log);
       process().defineVariable(m_t1_, Process::Mapping::exponential, log_lim_q2, "First incoming parton virtuality");
       process().defineVariable(m_t2_, Process::Mapping::exponential, log_lim_q2, "Second incoming parton virtuality");
     }
@@ -89,8 +89,8 @@ namespace cepgen {
     }
 
     double CollinearPhaseSpaceGenerator::fluxes() const {
-      return positiveFlux<CollinearFlux>().fluxQ2(process().x1(), m_t1_) / m_t1_ *
-             negativeFlux<CollinearFlux>().fluxQ2(process().x2(), m_t2_) / m_t2_;
+      return positiveFlux<CollinearFlux>().fluxQ2(process().x1(), m_t1_) *
+             negativeFlux<CollinearFlux>().fluxQ2(process().x2(), m_t2_);
     }
   }  // namespace proc
 }  // namespace cepgen
