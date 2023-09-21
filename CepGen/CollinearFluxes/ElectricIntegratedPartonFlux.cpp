4d272231e65245a6207f8ceb4bfac17fde94eda3
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-09-21 15:40:57 +0200

    Core: merged Electric/DZ integrated fluxes implementations

--- CepGen/CollinearFluxes/ElectricIntegratedPartonFlux.cpp
+++ CepGen/CollinearFluxes/ElectricIntegratedPartonFlux.cpp
@@ -24,18 +24,26 @@
 #include "CepGen/Utils/Message.h"
 
 namespace cepgen {
-  class ElectricIntegratedPartonFlux : public IntegratedPartonFlux {
+  class GenericElectricIntegratedPartonFlux : public IntegratedPartonFlux {
   public:
-    explicit ElectricIntegratedPartonFlux(const ParametersList& params) : IntegratedPartonFlux(params) {}
+    explicit GenericElectricIntegratedPartonFlux(const ParametersList& params)
+        : IntegratedPartonFlux(params),
+          q2_scale_(steer<double>("scale")),
+          polyn_coeff_(steer<std::vector<double> >("polynomialCoefficients")),
+          a_taming_(steer<bool>("aTaming")) {}
 
     double mass2() const override { return mp2_; }
-    bool fragmenting() const override final { return false; }
+    bool fragmenting() const override final { return true; }
     pdgid_t partonPdgId() const override final { return PDG::photon; }
 
     static ParametersDescription description() {
       auto desc = IntegratedPartonFlux::description();
-      desc.setDescription("Electric integ.flux");
+      desc.setDescription("Generic electric integ.flux");
       desc.add<pdgid_t>("pdgId", PDG::proton).setDescription("nucleon PDG id");
+      desc.add<double>("scale", 0.71)
+          .setDescription("scaling (in GeV^2) (0.71 for r_p = 0.81 fm, 0.66 for r_p = 0.84 fm)");
+      desc.add<std::vector<double> >("polynomialCoefficients", {-11. / 6, 3., -3. / 2, 1. / 3});
+      desc.add<bool>("aTaming", false);
       return desc;
     }
 
@@ -43,11 +51,42 @@ namespace cepgen {
       Limits q2range;
       if (!computeQ2range(x, q2range))
         return 0.;
-      const auto a = 1. + 0.71 / q2range.min(), inv_a = 1. / a;
-      return prefactor_ * (1. - x + 0.5 * x * x) / x *
-             ((a + 3.) / (a - 1.) * std::log(a) - 17. / 6 - 4. * inv_a / 3. + inv_a * inv_a / 6.);
+      const auto a = 1. + q2_scale_ / q2range.min(), inv_a = 1. / a;
+      double polyn = 0.;
+      for (size_t i = 0; i < polyn_coeff_.size(); ++i)
+        polyn += polyn_coeff_.at(i) * std::pow(inv_a, i);
+      const auto factor_a = a_taming_ ? (a + 3.) / (a - 1.) : 1.;
+      return prefactor_ * (1. - x + 0.5 * x * x) / x * (factor_a * std::log(a) + polyn);
+    }
+
+  private:
+    const double q2_scale_;
+    const std::vector<double> polyn_coeff_;
+    const bool a_taming_;
+  };
+
+  struct DreesZeppenfeldIntegratedPartonFlux final : public GenericElectricIntegratedPartonFlux {
+    using GenericElectricIntegratedPartonFlux::GenericElectricIntegratedPartonFlux;
+    static ParametersDescription description() {
+      auto desc = GenericElectricIntegratedPartonFlux::description();
+      desc.setDescription("Drees/Zeppenfeld integ.flux");
+      desc.add<std::vector<double> >("polynomialCoefficients", {-11. / 6, 3., -3. / 2, 1. / 3});
+      desc.add<bool>("aTaming", false);
+      return desc;
+    }
+  };
+
+  struct ElectricIntegratedPartonFlux final : public GenericElectricIntegratedPartonFlux {
+    using GenericElectricIntegratedPartonFlux::GenericElectricIntegratedPartonFlux;
+    static ParametersDescription description() {
+      auto desc = GenericElectricIntegratedPartonFlux::description();
+      desc.setDescription("Electric integ.flux");
+      desc.add<std::vector<double> >("polynomialCoefficients", {-17. / 6, -4. / 3, 1. / 6});
+      desc.add<bool>("aTaming", true);
+      return desc;
     }
   };
 }  // namespace cepgen
 
+REGISTER_INTEGRATED_PARTON_FLUX("DZ", DreesZeppenfeldIntegratedPartonFlux);
 REGISTER_INTEGRATED_PARTON_FLUX("Electric", ElectricIntegratedPartonFlux);
