7b72f495f20fe6dcfd7ab7be6d8ac4141ae82500
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-05-12 16:07:27 +0200

    Pythia8: more Lorentz boosts, and event hierarchy rewriting

--- CepGenAddOns/Pythia8Wrapper/LHEFHandler.cpp
+++ CepGenAddOns/Pythia8Wrapper/LHEFHandler.cpp
@@ -81,6 +81,7 @@ namespace cepgen {
         oss_init << std::endl;  // LHEF is usually not as beautifully parsed as a standard XML...
                                 // we're physicists, what do you expect?
         lhaevt_->addComments(oss_init.str());
+        lhaevt_->storeRemnants(true);
         lhaevt_->initialise(runParameters());
 #if PYTHIA_VERSION_INTEGER < 8300
         pythia_->setLHAupPtr(lhaevt_.get());
@@ -97,8 +98,7 @@ namespace cepgen {
       }
 
       inline bool operator<<(const Event& ev) override {
-        lhaevt_->feedEvent(compress_event_ ? ev : ev.compress(),
-                           pythia8::EventInterface::Type::centralAndFullBeamRemnants);
+        lhaevt_->feedEvent(compress_event_ ? ev : ev.compress());
         pythia_->next();
         lhaevt_->eventLHEF();
         return true;
